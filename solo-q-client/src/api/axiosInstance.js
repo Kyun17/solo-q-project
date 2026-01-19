bab664a565dae86878453373e90f817c72adf405
Geonwoo Lee <tldnrjsdn123@naver.com>
2026-01-19 23:50:07 +0900

    Update axiosInstance.js

--- solo-q-client/src/api/axiosInstance.js
+++ solo-q-client/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: VITE_API_URL, // 백엔드 주소
+  baseURL: import.meta.env.VITE_API_URL, // 백엔드 주소
   timeout: 5000,
   headers: {
     'Content-Type': 'application/json',
